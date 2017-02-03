31c9f2feea5605dac54cc0438648e7ae02f20894
coutomariel <coutomariel@gmail.com>
2017-02-03 09:10:13 -0200

    Carga inicial

---
+++ src/main/java/com/coutomariel/vinhos/VineSecondApplication.java
@@ -0,0 +1,12 @@
+package com.coutomariel.vinhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VineSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VineSecondApplication.class, args);
+	}
+}
