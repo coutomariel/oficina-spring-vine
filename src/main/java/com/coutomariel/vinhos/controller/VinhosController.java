1ca0b03adaf50e51194e5f09bc2292116ac6da89
coutomariel <coutomariel@gmail.com>
2017-02-07 16:03:27 -0200

    #5 - Implementada edição de vinhos

--- src/main/java/com/coutomariel/vinhos/controller/VinhosController.java
+++ src/main/java/com/coutomariel/vinhos/controller/VinhosController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -18,19 +20,21 @@ import com.coutomariel.vinhos.repository.Vinhos;
 import com.coutomariel.vinhos.repository.filter.VinhoFilter;
 
 @Controller
+@RequestMapping("/vinhos")
 public class VinhosController {
 
 	@Autowired
 	private Vinhos vinhos;
 	
-	@GetMapping("/vinhos/novo")
+	@GetMapping("/novo")
 	public ModelAndView novo(Vinho vinho){
 		ModelAndView mv = new ModelAndView("vinho/cadastro-vinho");
 		mv.addObject("tipos", TipoVinho.values());
+		mv.addObject("vinho", vinho);
 		return mv;
 	}
 	
-	@PostMapping("/vinhos/novo")
+	@PostMapping("/novo")
 	public ModelAndView salvar(@Valid Vinho vinho, BindingResult result, RedirectAttributes attributes){
 		if(result.hasErrors()){
 			return novo(vinho);
@@ -40,11 +44,17 @@ public class VinhosController {
 		return new ModelAndView("redirect:/vinhos/novo");
 	}
 	
-	@GetMapping("vinhos")
+	@GetMapping
 	public ModelAndView pesquisar(VinhoFilter vinhoFilter){
 		ModelAndView mv = new ModelAndView("vinho/pesquisa-vinho");
 		mv.addObject("vinhos", vinhos.findByNomeContainingIgnoreCase(
 				Optional.ofNullable(vinhoFilter.getNome()).orElse("%")));
 		return mv;
+	}
+	
+	@GetMapping("/{codigo}")
+	public ModelAndView editar(@PathVariable Long codigo){
+		Vinho vinho = vinhos.findOne(codigo);
+		return novo(vinho);
 	}
 }
